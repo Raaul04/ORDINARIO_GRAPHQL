b600a2755a4408f6cd83aee00eb28d0447e5945d
Raaul04 <raulletrado@hotmail.com>
2025-01-30 02:23:21 +0100

    update

---
+++ resolvers.ts
@@ -0,0 +1,11 @@
+
+
+export const resolvers = {
+
+    Query: {
+        default : ():string =>{
+            return "Hola Mundo"
+        },
+    },
+
+}
---
+++ types.ts
@@ -0,0 +1,1 @@
+import { ObjectId, OptionalId } from "mongodb";
