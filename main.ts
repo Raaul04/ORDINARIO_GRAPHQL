3b8890b5184e31896ee7b955f79d80761bc2aff1
Raaul04 <raulletrado@hotmail.com>
2025-01-30 02:36:40 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,30 +3,31 @@ import { schema } from "./schema.ts";
 import {  } from "./types.ts";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { resolvers } from "./resolvers.ts";
-//import { MongoClient } from "mongodb";
+import { MongoClient } from "mongodb";
 
-/*
+
 const MONGO_URL = Deno.env.get("MONGO_URL");
 
 if (!MONGO_URL) {
-  throw new Error("Please provide a MONGO_URL");
+  throw new Error("La variable de entorno 'mongoURL' no está configurada");
 }
 
 const mongoClient = new MongoClient(MONGO_URL);
 await mongoClient.connect();
-
 console.info("Connected to MongoDB");
 
-//const mongoDB = mongoClient.db("");
-//const c = mongoDB.collection<>("");
-*/
+
+const mongoDB = mongoClient.db("base de datos");
+
+//const XCollection = mongoDB.collection<modelo>("nombredeCollecion");
+
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
-  context: async () => ({  }),
+  context: async () => ({ /*Collecion */ }),
 });
 
 console.info(`Server ready at ${url}`);
