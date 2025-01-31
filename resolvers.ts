75c742709e9de94bead46fbe332fc643e3d87cb6
Raaul04 <raulletrado@hotmail.com>
2025-01-31 16:02:30 +0100

    Update resolvers.ts

--- resolvers.ts
+++ resolvers.ts
@@ -1,14 +1,29 @@
 import { RestaurantModel } from "./types.ts"
 import { GraphQLError } from "graphql";
 import { ObjectId,Collection } from "mongodb";
-
+type Context={
+    RestaurantCollection:Collection<RestaurantModel>
+}
+type ArgsGetRestaurant={
+    id:string
+}
 
 export const resolvers = {
+    Restaurant:{
+        id:(parent:RestaurantModel):string=>{
+            return parent._id!.toString();
+        }
+        //datetime
+
+    },
 
     Query: {
-        default : ():string =>{
-            return "Hola Mundo"
+        getRestaurant:async (_:unknown,args:ArgsGetRestaurant,ctx:Context):Promise<RestaurantModel|null> =>{
+            return await ctx.RestaurantCollection.findOne({args})
         },
+        getRestaurants:async(_:unknown,__:unknown, ctx:Context):Promise<RestaurantModel[]>=>{
+            return await ctx.RestaurantCollection.find().toArray()
+        }
     },
 
 }
